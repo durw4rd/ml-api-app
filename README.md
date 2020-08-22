ea0f9d47eba2cd7e65b5ce5426a568103e783c55
Michal Fasanek <misa.fasa@gmail.com>
2020-08-22 12:45:47 +0200

    mvp v2

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reading from Mavenlink API
+
+Not necessary to know all of this but thought I'd list some concepts that are useful to understand to make sense out of this:
+* [Understanding and using REST API](https://www.smashingmagazine.com/2018/01/understanding-using-rest-api/) - in case you're not familiar with what is discusses here, highly recommend to read the article
+* Node.js basics. There's plenty tutorials, I don't think it's necessary to do like a full course but it helps to get some sense what it is. [This](https://nodejs.dev/learn) content is directly from the ppl maintaining Node (I don't know if it's good).
+* HTTP methods used by RESTful APIs (GET, POST, PATCH,...) - covered in the first article
+* Promises & async-await functions in JavaScript
+* Template literals / template strings
--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+// Importing the axios package
 const axios = require('axios');
 
+// Setting request parameters (only authorization in this case)
 const OAuthToken = "680d29db876b116c77855e2defb074249efa3ecad04695c62af66ff9e31f7d3e"
-
 const config_get = {
     headers: {
         'Authorization': `Bearer ${OAuthToken}`
     }
 };
 
+// Talking to the API using the JS promise
 axios.get(`https://api.mavenlink.com/api/v1/time_entries.json`, config_get)
     .then(function (res) {
         let responseBody = res.data;
-        console.log(responseBody);
+        console.log(`### RESPONSE FROM THE PROMISE`)
+        console.log(`Response status: ${res.status} ${res.statusText}`)
+        console.log(`Number of returned time entries: ${responseBody.count}`);
+        console.log(responseBody.results);
+        console.log(responseBody.meta);
+    }).catch(e => {
+        console.log(`There has been a problem: ${e}`);
     });
 
+// Talking to the API using async-await function
+async function fetchTimeEntries() {
+    let responseFull = await axios.get(`https://api.mavenlink.com/api/v1/time_entries.json`, config_get);
+    let responseBody = responseFull.data;
+
+    console.log(`### RESPONSE FROM THE AYNC-AWAIT`)
+    console.log(`Response status: ${responseFull.status} ${responseFull.statusText}`);
+    console.log(`Number of returned time entries: ${responseBody.count}`);
+    console.log(responseBody.results);
+    console.log(responseBody.meta);
+}
+
+fetchTimeEntries()
+    .catch(e => {
+        console.log(`There has been a problem: ${e}`);
+    });
