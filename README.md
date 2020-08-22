b249bfd30cc2eceffbd4a10ecb00ea69b0fa0b48
Michal Fasanek <misa.fasa@gmail.com>
2020-08-22 13:39:59 +0200

    printing user objects

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Reading from Mavenlink API
 
 Not necessary to know all of this but thought I'd list some concepts that are useful to understand to make sense out of this:
-* [Understanding and using REST API](https://www.smashingmagazine.com/2018/01/understanding-using-rest-api/) - in case you're not familiar with what is discusses here, highly recommend to read the article
-* Node.js basics. There's plenty tutorials, I don't think it's necessary to do like a full course but it helps to get some sense what it is. [This](https://nodejs.dev/learn) content is directly from the ppl maintaining Node (I don't know if it's good).
+* Working with Git(Hub)
+* [Understanding and using REST API](https://www.smashingmagazine.com/2018/01/understanding-using-rest-api/) - in case you're not familiar with what is discussed here, highly recommend to read the article
+* Node.js basics. There's plenty of tutorials, I don't think it's necessary to do like a full course but it helps to get some sense what it is/how to work with it. [This](https://nodejs.dev/learn) content is directly from the ppl maintaining Node.
 * HTTP methods used by RESTful APIs (GET, POST, PATCH,...) - covered in the first article
 * Promises & async-await functions in JavaScript
 * Template literals / template strings
+
+To get this app 'running':
+1. Clone the repository: `git clone git@github.com:durw4rd/ml-api-app.git`
+2. (Inside the repo folder) `npm install`
+3. `node app.js` Will execute the file and print some stuff in the terminal.
--- app.js
+++ app.js
@@ -10,28 +10,62 @@ const config_get = {
 };
 
 // Talking to the API using the JS promise
-axios.get(`https://api.mavenlink.com/api/v1/time_entries.json`, config_get)
-    .then(function (res) {
-        let responseBody = res.data;
-        console.log(`### RESPONSE FROM THE PROMISE`)
-        console.log(`Response status: ${res.status} ${res.statusText}`)
-        console.log(`Number of returned time entries: ${responseBody.count}`);
-        console.log(responseBody.results);
-        console.log(responseBody.meta);
-    }).catch(e => {
-        console.log(`There has been a problem: ${e}`);
-    });
+// axios.get(`https://api.mavenlink.com/api/v1/time_entries.json`, config_get)
+//     .then(function (res) {
+//         let responseBody = res.data;
+//         console.log(`### RESPONSE FROM THE PROMISE`)
+//         console.log(`Response status: ${res.status} ${res.statusText}`)
+//         console.log(`Number of returned time entries: ${responseBody.count}`);
+//         console.log(responseBody.results);
+//         console.log(responseBody.meta);
+//     }).catch(e => {
+//         console.log(`There has been a problem: ${e}`);
+//     });
 
 // Talking to the API using async-await function
 async function fetchTimeEntries() {
-    let responseFull = await axios.get(`https://api.mavenlink.com/api/v1/time_entries.json`, config_get);
+    // getting current date/time
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = ('0' + today.getMonth()).slice(-2);
+    let day = ('0' + today.getDate()).slice(-2);
+    let fullDateYesterday = `${year}-${month}-${day - 1}`
+    let dayStart = 'T000000';
+    let dayEnd = 'T115959';
+    let singleDayParam = `${fullDateYesterday}${dayStart}:${fullDateYesterday}${dayEnd}`;
+    // console.log(singleDayParam);
+
+    // set filter parameters
+    let includeParam = 'include=user';
+    let timeFilter = `created_at_between=${singleDayParam}`;
+
+    let responseFull = await axios.get(`https://api.mavenlink.com/api/v1/time_entries.json?${includeParam}&${timeFilter}`, config_get);
     let responseBody = responseFull.data;
 
     console.log(`### RESPONSE FROM THE AYNC-AWAIT`)
+
+    /* Printing out different pieces of the response - helps to get sense of what data is available 
+       and what's their structure. (At the same time, this is probably easier done in Postman.)
+    */
     console.log(`Response status: ${responseFull.status} ${responseFull.statusText}`);
     console.log(`Number of returned time entries: ${responseBody.count}`);
     console.log(responseBody.results);
+    console.log(responseBody.time_entries);
+    console.log(responseBody.users);
     console.log(responseBody.meta);
+
+    // Create array with users who added an entry (yesterday)
+    const userObjectValues = Object.values(responseBody.users)
+    let userArray = [];
+    userObjectValues.forEach((user) => {
+        let userObject = {
+            'name': user.full_name,
+            'email': user.email_address
+        };
+        userArray.push(userObject);
+    });
+    console.log(`### PRINTING USERS WHO ENTERED TIME YESTERDAY`);
+    console.log(userArray);
 }
 
 fetchTimeEntries()
